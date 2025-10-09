f66a88003104cb17ca35106fe3d5907bf05052a2
Prasanth-P29 <prasanth477361@gmail.com>
2025-10-09 21:50:29 +0530

    code

--- src/Basics/undefined.ts
+++ src/Basics/undefined.ts
@@ -1,5 +1,5 @@
 //Q4. Undefined and Null
-let a1: undefined = undefined; // Undefined
-let b1: null = null; // Null
+let p: undefined = undefined; // Undefined
+let q: null = null; // Null
 
-console.log("Q4.", a1, b1);
+console.log("Q4.", p, q);
